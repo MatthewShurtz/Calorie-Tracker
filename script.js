12c9ad216562e20db3640b242b59f5186284aa8f
MatthewShurtz <59268254+MatthewShurtz@users.noreply.github.com>
2020-10-02 16:02:36 -0500

    MVP completed

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const sumBtn = document.getElementById("sum-btn");
 // Adds both food item and number of calories to list then clears the field.
 addBtn.addEventListener("click", addItem);
 addBtn.addEventListener("click", clearField);
+sumBtn.addEventListener("click", calSum);
 
 function clearField() {
   document.getElementById("food-input").value = "";
@@ -13,6 +14,7 @@ function clearField() {
 function addItem() {
   let newFoodItem = document.createElement("li");
   let newCalNum = document.createElement("li");
+  newCalNum.className = "calories";
   let food = document.getElementById("food-input").value;
   let cal = document.getElementById("calorie-input").value;
   let addedFood = document.createTextNode(food);
@@ -25,11 +27,11 @@ function addItem() {
   calList.appendChild(newCalNum);
 }
 
-// function addCal() {
-//   let newCalNum = document.createElement("li");
-//   let cal = document.getElementById("calorie-input").value;
-//   let addedCal = document.createTextNode(cal);
-//   newCalNum.appendChild(addedCal);
-//   let calList = document.getElementById("calorie-number-list");
-//   calList.appendChild(newCalNum);
-// }
+function calSum() {
+  let calories = document.getElementsByClassName("calories");
+  calNum = 0;
+  for (var i = 0; i < calories.length; i++) {
+    calNum += parseInt(calories[i].textContent);
+  }
+  document.getElementById("total-calories").innerHTML = calNum;
+}
